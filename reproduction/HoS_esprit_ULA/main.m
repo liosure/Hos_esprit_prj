7d9bf4183d9b2655b0a53457206cbb3477e11eef
liosure <34795823+liosure@users.noreply.github.com>
2024-12-24 11:19:06 +0800

    1

--- reproduction/HoS_esprit_ULA/main.m
+++ reproduction/HoS_esprit_ULA/main.m
@@ -1,6 +1,6 @@
 clear;close all
 str_vec = @(r) exp(1j*2*pi*r);
-L = 5;
+L = 2;
 K_a_half = 64;
 K_a = K_a_half*2+1; K_e = 1;
 fc = 2.8e10;
@@ -14,14 +14,13 @@ diff_dis = zeros(K_a*K_e,L);
 Ant_res = zeros(K_a*K_e,L);
 
 rcs_power = 10;
-P_noise = 0;
+P_noise = -100;
 
 ant_pos = [zeros(K_a*K_e,1),kron((0:K_a-1)'*d_a,ones(K_e,1)), kron(ones(K_a,1),(0:K_e-1)'*d_e)];
-tar_pos = [30,15,3;...
+tar_pos = [10,20,1;
+    14,30,2;30,15,3;...
     10,-2,2;
-    20,-15,2;
-    10,20,1;
-    15,0,0];
+    20,-15,2;];
 for i = 1:L
     diff_tar_rec(:,:,i) = tar_pos(i,:)-ant_pos;
     azi(:,i) = atan(diff_tar_rec(:,2,i)./diff_tar_rec(:,1,i));
@@ -50,14 +49,10 @@ theta = (-0.5:1/400:0.5)*2;
 test_range = -(-K_a_half:K_a_half)'.*permute(theta'*ones(size(r)),[3,1,2])/2 +...
      d_a^2/2/lambda*((-K_a_half:K_a_half).^2)'.*permute((1-theta.^2)'./r,[3,1,2]);
 t_bas_2nd = str_vec(test_range);
-P_mu = zeros(401,501);
-for i = 1:401
-    for j = 1:501
-        P_mu(i,j) = (t_bas_2nd(:,i,j)' * (U_noise*U_noise') * t_bas_2nd(:,i,j));
-    end
-end
+P_mu = abs(permute(sum(abs(pagemtimes(permute(conj(t_bas_2nd),[2,1,3]), U_noise)).^2,2),[1,3,2]));
 P_pod = 1./P_mu;
-[a,pc] = polarPcolor(r,asin(theta)/pi*180,100*abs(P_pod)/max(max(abs(P_pod))),'colBar',0);
+subplot(1,3,1)
+[a,pc] = polarPcolor(r,asin(theta)/pi*180,50*log(100*abs(P_pod)/max(max(abs(P_pod))))/log(10),'colBar',0);
 scatter(sin(azi(65,:)).*diff_dis(65,:)/far_limit,cos(azi(65,:)).*diff_dis(65,:)/far_limit, 'r*');
 r_est = zeros(Num_d,1);
 theta_est = zeros(Num_d,1);
@@ -70,18 +65,12 @@ for count = 1:Num_d
     v_recon = str_vec(-(-K_a_half:K_a_half)'*stheta/2 +...
     d_a^2/2/lambda*((-K_a_half:K_a_half).^2)'*(1-stheta^2)'/r_est(count));
     P_mu = P_mu+abs(permute(pagemtimes(permute(conj(t_bas_2nd),[2,1,3]),v_recon),[1,3,2])).^2;
-
+    P_pod = 1./P_mu;
+    subplot(1,3,count+1)
+    [a,pc] = polarPcolor(r,asin(theta)/pi*180,50*log(100*abs(P_pod)/max(max(abs(P_pod))))/log(10),'colBar',0);
+    scatter(sin(azi(65,:)).*diff_dis(65,:)/far_limit,cos(azi(65,:)).*diff_dis(65,:)/far_limit, 'r*');
+    scatter(r_est(1:count).*sin(theta_est(1:count))/far_limit,r_est(1:count).*cos(theta_est(1:count))/far_limit, 'go');
 end
-scatter(r_est.*sin(theta_est)/far_limit,r_est.*cos(theta_est)/far_limit, 'go');
-% %% DFT
-% P_dft = zeros(401,501);
-% for i = 1:401
-%     for j = 1:501
-%         P_dft(i,j) = sqrt(sum(abs(t_bas_2nd(:,i,j)'*rec_sig).^2,2));
-%     end
-% end
-% figure()
-% [a,pc] = polarPcolor(r,asin(theta)/pi*180,abs(P_dft)/max(max(abs(P_dft))));
-% title('DFT')
+
 
 
