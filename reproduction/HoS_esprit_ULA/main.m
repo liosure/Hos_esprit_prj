9452cec4ba98d486b1f3ed779d16c9e0e3730952
liosure <34795823+liosure@users.noreply.github.com>
2025-01-02 21:16:45 +0800

    0

--- reproduction/HoS_esprit_ULA/main.m
+++ reproduction/HoS_esprit_ULA/main.m
@@ -3,7 +3,7 @@ str_vec = @(r) exp(1j*2*pi*r);
 L = 5; K_a_half = 64;
 K_a = K_a_half*2+1; K_e = 1;
 fc = 2.8e10; c = 3e8; lambda = c/fc;
-d_a = 1/2*lambda; d_e = 1/2*lambda;diff_tar_rec = zeros(K_a*K_e,3,L);
+d_a = 1*lambda; d_e = 1/2*lambda;diff_tar_rec = zeros(K_a*K_e,3,L);
 azi = zeros(K_a*K_e,L);ele = zeros(K_a*K_e,L);
 diff_dis = zeros(K_a*K_e,L);Ant_res = zeros(K_a*K_e,L);
 
@@ -11,6 +11,7 @@ rcs_power = 10; P_noise = -100;
 
 ant_pos = [zeros(K_a*K_e,1),kron((0:K_a-1)'*d_a,ones(K_e,1)), kron(ones(K_a,1),(0:K_e-1)'*d_e)];
 tar_pos = [10,20,1; 14,30,2; 30,15,3; 10,-2,2; 20,-15,2;];
+
 for i = 1:L
     diff_tar_rec(:,:,i) = tar_pos(i,:)-ant_pos;
     azi(:,i) = atan(diff_tar_rec(:,2,i)./diff_tar_rec(:,1,i));
@@ -36,7 +37,7 @@ far_limit = d_a^2*K_a^2/lambda;
 sense_limit = 0.62*sqrt(d_a^3/lambda);
 r = (0:1/500:1)*(far_limit-sense_limit)+sense_limit;
 theta = (-0.5:1/400:0.5)*2;
-test_range = -(-K_a_half:K_a_half)'.*permute(theta'*ones(size(r)),[3,1,2])/2 +...
+test_range = -(-K_a_half:K_a_half)'.*permute(theta'*ones(size(r)),[3,1,2])*d_a/lambda +...
      d_a^2/2/lambda*((-K_a_half:K_a_half).^2)'.*permute((1-theta.^2)'./r,[3,1,2]);
 t_bas_2nd = str_vec(test_range);
 P_mu = abs(permute(sum(abs(pagemtimes(permute(conj(t_bas_2nd),[2,1,3]), U_noise)).^2,2),[1,3,2]));
@@ -56,7 +57,7 @@ for count = 1:Num_d
     r_est(count) = floor(idx/401)/500*(far_limit-sense_limit)+sense_limit;
     stheta = (mod(idx-1, 401)-200)/200;
     theta_est(count) = asin((mod(idx-1, 401)-200)/200);
-    v_recon = str_vec(-(-K_a_half:K_a_half)'*stheta/2 + d_a^2/2/lambda*((-K_a_half:K_a_half).^2)'*(1-stheta^2)'...
+    v_recon = str_vec(-(-K_a_half:K_a_half)'*stheta*d_a/lambda + d_a^2/2/lambda*((-K_a_half:K_a_half).^2)'*(1-stheta^2)'...
         /r_est(count));
     P_mu = P_mu+abs(permute(pagemtimes(permute(conj(t_bas_2nd),[2,1,3]),v_recon),[1,3,2])).^2;
     P_pod = 1./P_mu;
